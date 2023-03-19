55a6b4961ce40115477e3b9cab0d92eb8bfc7035
Sarvenaz Salehi Mourkani <sarvenaz.salehi_mourkani@mercedes-benz.com>
2023-03-19 07:23:52 +0000

    created split modified to split dataset added the first config file

--- create_splits.py
+++ create_splits.py
@@ -2,10 +2,11 @@ import argparse
 import glob
 import os
 import random
-
+import shutil
 import numpy as np
+import sklearn.model_selection
 
-from utils import get_module_logger
+train_test_split= sklearn.model_selection.train_test_split
 
 
 def split(source, destination):
@@ -17,7 +18,20 @@ def split(source, destination):
         - source [str]: source data directory, contains the processed tf records
         - destination [str]: destination data directory, contains 3 sub folders: train / val / test
     """
-    # TODO: Implement function
+    # take all the tfrecord filename from source
+    dataset = glob.glob(source+'/*.tfrecord') 
+    #default percentage for train is 75%
+    train, val_tr = train_test_split(dataset, test_size=0.2)
+    #from 25% of the whole 75% is stored for test and the rest for validation
+    test, val = train_test_split(val_tr)
+    # create paths for storing train, val and test files
+    for file_path in train:
+        shutil.move(file_path, os.path.join(destination, "train",os.path.basename(file_path)))
+    for file_path in test:
+        shutil.move(file_path, os.path.join(destination, "test",os.path.basename(file_path)))
+    for file_path in val:
+        shutil.move(file_path, os.path.join(destination, "val",os.path.basename(file_path)))
+     
 
 
 if __name__ == "__main__":
@@ -28,6 +42,4 @@ if __name__ == "__main__":
                         help='destination data directory')
     args = parser.parse_args()
 
-    logger = get_module_logger(__name__)
-    logger.info('Creating splits...')
     split(args.source, args.destination)
